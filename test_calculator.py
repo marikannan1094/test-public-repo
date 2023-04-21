e522886baacd0afab69fcca4fb4932faa32cf919
Mari Kannan Balasubramaniam <marikannan1094@gmail.com>
2023-04-21 18:24:38 -0500

    Initial commit

---
+++ test_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+  
+    def test_add(self):
+        '''Test case function for addition'''
+        self.calc = Calculator()
+        result = self.calc.add(4, 7)
+        expected = 11
+        self.assertEqual(result, expected)
+
+    def test_sub(self):
+        '''Test case function for subtraction'''
+        self.calc = Calculator()
+        result = self.calc.sub(10, 5)
+        expected = 5
+        self.assertEqual(result, expected)
+
+    @unittest.skip('Some reason')
+    def test_mul(self):
+        '''Test case function for multiplication'''
+        self.calc = Calculator()
+        result = self.calc.mul(3, 7)
+        expected = 21
+        self.assertEqual(result, expected)
+
+    def test_div(self):
+        '''Test case function for division'''
+        self.calc = Calculator()
+        result = self.calc.div(10, 2)
+        expected = 4
+        self.assertEqual(result, expected)
